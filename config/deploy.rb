c6c3759340f0070e7f94bf1b157c30884272e76c
Christophe <christophe@faivre.co.za>
2012-01-15 20:43:54 +0200

    Initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+set :application, "ITM API"
+set :repository,  "git@github.com:cfaivre/ships_admin"
+
+set :scm, :git
+
+set :user, "itmapi"
+set :port, 22
+set :deploy_to, "/srv/www/itm-api.de"
+set :use_sudo, false
+set :keep_releases, 5
+set :bundle_flags, "--deployment --quiet --binstubs"
+
+role :web, "87.106.53.59"                          # Your HTTP server, Apache/etc
+role :app, "87.106.53.59"                          # This may be the same as your `Web` server
+role :db,  "87.106.53.59", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShipsAdmin::Application.config.secret_token = '2724b724f77d80b836d231e8fef9473bd253ad97b8c1da865b1062c29121a0d6f719fd714cf6794a577b2145a0ef499a99a662277690577210c0878306c1f4e6'
