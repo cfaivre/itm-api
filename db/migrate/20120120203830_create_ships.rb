c08ef620df37e42c5ae689151c4e5798e2262152
Christophe <christophe@faivre.co.za>
2012-01-20 22:40:11 +0200

    Add ship model

---
+++ db/migrate/20120120203830_create_ships.rb
@@ -0,0 +1,13 @@
+class CreateShips < ActiveRecord::Migration
+  def change
+    create_table :ships do |t|
+      t.string :name
+      t.string :short_name
+      t.integer :shipping_company_id
+      t.integer :shipping_type_id
+      t.integer :sid
+
+      t.timestamps
+    end
+  end
+end
