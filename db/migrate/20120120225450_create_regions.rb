e25c98af29db67bfe4fe040cce09e047428c8912
Christophe <christophe@faivre.co.za>
2012-01-21 01:18:43 +0200

    Fix migration file

--- db/migrate/20120120225450_create_regions.rb
+++ db/migrate/20120120225450_create_regions.rb
@@ -6,23 +6,23 @@ class CreateRegions < ActiveRecord::Migration
 
       t.timestamps
     end
+    #Populate default data
+    Region.create :name => "Afrika"
+    Region.create :name => "Nordamerika"
+    Region.create :name => "Südamerika"
+    Region.create :name => "Amerika"
+    Region.create :name => "Nordpol / Südpol"
+    Region.create :name => "Asien"
+    Region.create :name => "Australien / Neuseeland"
+    Region.create :name => "Südeuropa / Mittelmeer"
+    Region.create :name => "Westeuropa"
+    Region.create :name => "Nordeuropa"
+    Region.create :name => "Osteuropa"
+    Region.create :name => "Deutschland"
+    Region.create :name => "Südeuropa"
+    Region.create :name => "Pazifik"
+    Region.create :name => "Mittelamerika / Karibik"
+    Region.create :name => "Transreisen"
+    Region.create :name => "Weltreise"
   end
-  #Populate default data
-  Region.create :name => "Afrika"
-  Region.create :name => "Nordamerika"
-  Region.create :name => "Südamerika"
-  Region.create :name => "Amerika"
-  Region.create :name => "Nordpol / Südpol"
-  Region.create :name => "Asien"
-  Region.create :name => "Australien / Neuseeland"
-  Region.create :name => "Südeuropa / Mittelmeer"
-  Region.create :name => "Westeuropa"
-  Region.create :name => "Nordeuropa"
-  Region.create :name => "Osteuropa"
-  Region.create :name => "Deutschland"
-  Region.create :name => "Südeuropa"
-  Region.create :name => "Pazifik"
-  Region.create :name => "Mittelamerika / Karibik"
-  Region.create :name => "Transreisen"
-  Region.create :name => "Weltreise"
 end
