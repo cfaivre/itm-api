3b2d43ca36efcaade56c356a2e1a51ee74f672dc
Christophe <christophe@faivre.co.za>
2012-01-20 21:32:27 +0200

    Changed branch from production to development

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,4 @@ role :app, "87.106.53.59"                          # This may be the same as you
 role :db,  "87.106.53.59", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
                                                                                                                                                                                                                                                                                
-set :branch, "production"
+set :branch, "development"
