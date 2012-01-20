66c2ef912c862c915fd3eca03428cdb4ea634ea5
Christophe <christophe@faivre.co.za>
2012-01-20 22:36:13 +0200

    Added shipping company to active admin as resource

---
+++ app/admin/shipping_companies.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register ShippingCompany do
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120113224814) do
+ActiveRecord::Schema.define(:version => 20120120202053) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -45,5 +45,19 @@ ActiveRecord::Schema.define(:version => 20120113224814) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "ship_types", :force => true do |t|
+    t.string   "description"
+    t.string   "short_description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "shipping_companies", :force => true do |t|
+    t.string   "name"
+    t.integer  "rid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
