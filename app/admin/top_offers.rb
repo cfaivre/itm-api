e7ad71599cc2582b6325fe340e5e6ec0f34b4df9
Christophe <christophe@faivre.co.za>
2012-01-23 22:04:34 +0200

    More changes to topoffers

--- app/admin/top_offers.rb
+++ app/admin/top_offers.rb
@@ -7,19 +7,28 @@ ActiveAdmin.register TopOffer do
     column "Maximum Number of Days", :maximum_number_of_days
     column "Price", :price
     column "Link to more info", :link_to_more_info
-    column "Offer Start Date", :offer_start_date
-    column "Offer End Date", :offer_end_date
-    column "Request Offer From", :request_offer_from
-    column "Request Offer Until", :request_offer_until
+    column "Offer Start Date", :offer_start_date, :as => :datepicker
+    column "Offer End Date", :offer_end_date, :as => :datepicker
+    column "Request Offer From", :request_offer_from, :as => :datepicker
+    column "Request Offer Until", :request_offer_until, :as => :datepicker
     column "Image" do |top_offer|
       link_to image_tag(top_offer.image.url(:thumb))#, admin_post_path(image)
     end
     default_actions
   end
 
-#  <%= image_tag @user.avatar.url %>
-#<%= image_tag @user.avatar.url(:medium) %>
-#<%= image_tag @user.avatar.url(:thumb) %>
+  show do |top_offer|
+    attributes_table_for top_offer do
+      [:description, :detailed_description, :minimum_number_of_days, :maximum_number_of_days,
+       :price, :link_to_more_info, :offer_start_date, :offer_end_date, :request_offer_from, :request_offer_until].each do |column|
+          row column
+        end
+    end
+    div do
+      link_to image_tag(top_offer.image.url(:thumb))#, admin_post_path(image)
+    end
+    active_admin_comments
+  end
 
   form :html => { :enctype => "multipart/form-data" } do |f|
     f.inputs "Details" do
@@ -29,13 +38,13 @@ ActiveAdmin.register TopOffer do
       f.input :detailed_description
       f.input :minimum_number_of_days
       f.input :maximum_number_of_days
-      f.input :price, :as => :date
+      f.input :price
       f.input :link_to_more_info, :as => :url
-      f.input :offer_start_date, :as => :date
+      f.input :offer_start_date, :as => :datepicker
+      f.input :offer_end_date, :as =>:datepicker
+      f.input :request_offer_from, :as => :datepicker
+      f.input :request_offer_until, :as => :datepicker
       f.input :image, :as => :file
-      f.input :offer_end_date, :as =>:date
-      f.input :request_offer_from, :as => :date
-      f.input :request_offer_until, :as => :date
     end
     f.buttons
   end
