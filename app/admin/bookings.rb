9e06836bb9e82a1a578ef1deb8d495390189446a
Christophe <christophe@faivre.co.za>
2012-02-14 22:13:54 +0200

    Changes to customers and bookings

--- app/admin/bookings.rb
+++ app/admin/bookings.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register Booking do
-  
+  form do |f|
+    f.inputs "Details" do
+      f.input :customer, :as => :select, :collection => Customer.all
+      f.input :booking_number
+      f.input :emergency_contact_first_name
+      f.input :emergency_contact_last_name
+      f.input :emergency_contact_telephone
+      f.input :ship, :as => :select, :collection => Ship.all
+      f.input :travel_date, :as => :datepicker
+      f.input :total_price
+      f.input :how_to_get_at_ship
+      f.input :coming_from
+      f.input :eating_session
+      f.input :other
+    end
+    f.buttons
+  end
 end
--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,3 +1,44 @@
 ActiveAdmin.register Customer do
-  
+=begin
+    form do |f|
+    f.inputs "Details" do # customer's fields
+      f.input :first_name
+      f.input :last_name
+    end
+
+    f.has_many :bookings do |booking|
+      booking.inputs "Bookings" do
+        if !booking.object.nil?
+          # show the destroy checkbox only if it is an existing booking
+          # else, there's already dynamic JS to add / remove new bookingss
+          booking.input :_destroy, :as => :boolean, :label => "Destroy?"
+        end
+
+        booking.input :ship_id # it should automatically generate a drop-down select to choose from your existing patients
+        booking.input :booking_number
+      end
+    end
+    end
+=end
+  form do |f|
+    f.inputs "Details" do # customer's fields
+      f.input :username
+      f.input :password
+      f.input :first_name
+      f.input :last_name
+      f.input :gender
+      f.input :date_of_birth, :as => :datepicker
+      f.input :email_address
+      f.input :telephone
+      f.input :cellphone
+      f.input :fax
+      f.input :street_address
+      f.input :suburb
+      f.input :postcode
+      f.input :city
+      f.input :state
+      f.input :country
+    end
+    f.buttons
+  end
 end
