264e5dd5d08fe30a15d4f7d2f9c3bfc2e07c6d7b
Christophe <christophe@faivre.co.za>
2012-02-20 22:53:13 +0200

    Adding passengers on bookings screen

--- app/admin/bookings.rb
+++ app/admin/bookings.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 ActiveAdmin.register Booking do
   menu :if => proc{ can?(:manage, Booking) }
   form do |f|
@@ -12,9 +13,31 @@ ActiveAdmin.register Booking do
       f.input :total_price
       f.input :how_to_get_at_ship
       f.input :coming_from
-      f.input :eating_session
+      f.input :eating_session, :as => :radio, :collection => ["Früh", "Spät"]
+
       f.input :other
     end
+
+    f.has_many :passengers do |pass_f|
+      pass_f.inputs "Passengers" do
+        if !pass_f.object.nil?
+          # show the destroy checkbox only if it is an existing passenger
+          # else, there's already dynamic JS to add / remove new passengers
+          pass_f.input :_destroy, :as => :boolean, :label => "Destroy?"
+        end
+        pass_f.input :first_name
+        pass_f.input :last_name
+        pass_f.input :nationality
+        pass_f.input :date_of_birth
+        pass_f.input :place_of_birth
+        pass_f.input :passport_number
+        pass_f.input :passport_date_issued
+        pass_f.input :passport_place_issued
+        pass_f.input :passport_valid_until
+      end
+    end
+
     f.buttons
   end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ActiveRecord::Base
   belongs_to :customer
   belongs_to :ship
+  has_many :passengers
+  accepts_nested_attributes_for :passengers, :allow_destroy => true
 end
